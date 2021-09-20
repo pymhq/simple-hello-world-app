6ea2f349e110dadfb14b52f5bff450759aa38cf3
Andy Peng <80087186+pym91@users.noreply.github.com>
2021-09-19 21:58:19 -0700

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,13 +1,45 @@
+from flask import render_template
 from flask import Flask
-import os
-import time
+
+from opentelemetry import trace
+from opentelemetry.exporter.otlp.proto.grpc.trace_exporter import OTLPSpanExporter
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+
+from opentelemetry.sdk.extension.aws.trace import AwsXRayIdGenerator
+
+from opentelemetry import propagate
+from opentelemetry.sdk.extension.aws.trace.propagation.aws_xray_format import AwsXRayFormat
+
+from opentelemetry.instrumentation.botocore import BotocoreInstrumentor
+from opentelemetry.instrumentation.requests import RequestsInstrumentor
+from opentelemetry.instrumentation.flask import FlaskInstrumentor
+
+otlp_exporter = OTLPSpanExporter(endpoint="http://localhost:4317")
+span_processor = BatchSpanProcessor(otlp_exporter)
+trace.set_tracer_provider(TracerProvider(active_span_processor=span_processor, id_generator=AwsXRayIdGenerator()))
+
+propagate.set_global_textmap(AwsXRayFormat())
+
 app = Flask(__name__)
+
+
+BotocoreInstrumentor().instrument()
+# Initialize `Instrumentor` for the `requests` library
+RequestsInstrumentor().instrument()
+# Initialize `Instrumentor` for the `flask` web framework
+FlaskInstrumentor().instrument_app(app)
+
 @app.route('/')
-def hello_world():
-    time.sleep(0.5)
-    return 'Hello World! Startdeployment V1'
-@app.route('/health')
-def health_check():
-    return 'health check'
+def home():
+    return render_template('index.html')
+
+@app.route('/app')
+def blog():
+    return "Hello, from App!"
+
+
+
+
 if __name__ == '__main__':
-    app.run(threaded=True, host="0.0.0.0", debug=True, port=int(os.environ.get("PORT", 8000)))
+    app.run(threaded=True,host='0.0.0.0',port=8081)
